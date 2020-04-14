3691324215f9a5aac31fa1eeb9b4decb14e35789
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-04-14 09:14:46 +0900

    감정저금통-ui

---
+++ ui/Form2.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace coc
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("조선일보명조", 25F, System.Drawing.FontStyle.Bold);
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(217)))), ((int)(((byte)(226)))));
+			this.label1.Location = new System.Drawing.Point(334, 678);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(423, 43);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "이어폰을 착용해주세요";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.pictureBox1.Location = new System.Drawing.Point(327, 214);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("조선일보명조", 25F, System.Drawing.FontStyle.Bold);
+			this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(87)))), ((int)(((byte)(111)))));
+			this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+			this.button1.Location = new System.Drawing.Point(660, 775);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(198, 61);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "NEXT";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form2
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+			this.ClientSize = new System.Drawing.Size(1000, 1000);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form2";
+			this.Text = "Form2";
+			this.Load += new System.EventHandler(this.Form2_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}

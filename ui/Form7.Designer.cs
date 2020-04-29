bb8e8c15ceaa631e472f32cafdeb26fb884e8471
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-04-29 22:26:28 +0900

    감정저금통-ui

---
+++ ui/Form7.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace coc
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("조선일보명조", 30F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(199)))), ((int)(((byte)(182)))));
+            this.label2.Location = new System.Drawing.Point(189, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(383, 51);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "뇌가 정한 관심사";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(81, 87);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("조선일보명조", 30F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(217)))), ((int)(((byte)(226)))));
+            this.label1.Location = new System.Drawing.Point(-37, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(796, 51);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "10초 후 관련 영상을 시작하겠습니다";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("조선일보명조", 30F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(217)))), ((int)(((byte)(226)))));
+            this.label3.Location = new System.Drawing.Point(29, 315);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(493, 51);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "결과를 출력 중 입니다";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(501, 500);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ui/Form7.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coc
+{
+    public partial class Form7 : Form
+    {
+        private int t = 0;
+        private string[] ss;
+        private System.Windows.Forms.Timer time = new System.Windows.Forms.Timer();
+		String[] ll = { "C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\image\\가족.png",
+				"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\image\\동물.png",
+				"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\image\\건강.png",
+				"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\image\\옷.png"};
+		private int[] l = new int[16];
+        public static int be = 0; //몇번 사진인지
+        public Form7()
+        {
+            InitializeComponent();
+			//Form6 f6 = new Form6();
+			//int[] f = f6.l5;
+			int[] f = Form6.l5;
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Controls.Remove(label1);//10초후
+            this.Controls.Remove(label2);//뇌가 선택한
+            this.Controls.Remove(pictureBox1);//선택사진
+            while (true)
+            {
+                if (System.IO.File.Exists(@"C:\Users\sohyeon\Desktop\내보내기.txt") == true)
+                {
+                    ss = System.IO.File.ReadAllLines(@"C:\Users\sohyeon\Desktop\내보내기.txt");  //몇번 사진이 채택되었는지 받아옴
+                    be=f[Convert.ToInt32(ss[0])]-1;
+                    pictureBox1.ImageLocation = ll[be];
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                }
+            }
+			label3.Left = (this.ClientSize.Width - label3.Width) / 2;
+            label3.Top = (this.ClientSize.Height - label3.Height) / 2;
+            time.Interval = 1000;
+            time.Tick += new EventHandler(time_Tick);
+            time.Start();
+
+
+        }
+        private void time_Tick(object sender, EventArgs e)
+        {
+            t++;
+            if (t == 3)
+            {
+                this.Controls.Remove(label3);
+                this.Controls.Add(label2);
+                this.Controls.Add(pictureBox1);
+                pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+                pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+                label2.Left = (this.ClientSize.Width - label2.Width) / 2;
+                label2.Top = (this.ClientSize.Height - label2.Height) / 2 - 400;
+            }
+            if (t == 10)
+            {
+                this.Controls.Remove(pictureBox1);
+                this.Controls.Remove(label2);
+                this.Controls.Add(label1);
+                label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+                label1.Top = (this.ClientSize.Height - label1.Height) / 2;
+            }
+            if (t == 20)
+            {
+                time.Enabled = false;
+                time.Stop();
+                Form8 showForm8 = new Form8();
+                showForm8.ShowDialog();
+                this.Visible = false;
+                Close();
+            }
+        }
+    }
+}

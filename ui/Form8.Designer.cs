56f32e97f054a54261850935fccded171d33f758
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-05-01 15:37:56 +0900

    감정저금통-ui

---
+++ ui/Form8.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace coc
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form8));
+            this.label1 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("조선일보명조", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(217)))), ((int)(((byte)(226)))));
+            this.label1.Location = new System.Drawing.Point(198, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 43);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "뇌파 측정 중";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(22, 51);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1000, 600);
+            this.axWindowsMediaPlayer1.TabIndex = 12;
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(571, 625);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form8";
+            this.Text = "Form8";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
---
+++ ui/Form8.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coc
+{
+    public partial class Form8 : Form
+    {
+        private System.Windows.Forms.Timer time = new System.Windows.Forms.Timer();
+        private System.Windows.Forms.Timer time1 = new System.Windows.Forms.Timer();
+        private Random r = new Random(DateTime.Now.Millisecond);
+		private string[] ss;
+		public static int g = 0, a = 0; //몇번 사진인지,a는 몇번 동영상인지
+        String[] ll = { "C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\가족1.mp4",
+					"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\가족2.mp4",
+					"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\동물.mp4",
+					"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\건강1.mp4",
+					"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\건강2.mp4",
+					"C:\\Users\\sohyeon\\Desktop\\소현\\전공\\신경공학(특별학기)\\바름조_최종\\video\\옷.mp4"};
+        public Form8()
+        {
+            InitializeComponent();
+			string[] q = { "3" };
+			System.IO.File.AppendAllLines(@"C:\Users\sohyeon\Desktop\불러오기.txt", q);  //동영상이 시작됨을 알림
+			g = Form7.be;  //몇번 사진인지 폼7에서 받아옴
+			this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = (this.ClientSize.Height - label1.Height) / 2 + 400;
+            axWindowsMediaPlayer1.Left = (this.ClientSize.Width - axWindowsMediaPlayer1.Width) / 2;
+            axWindowsMediaPlayer1.Top = (this.ClientSize.Height - axWindowsMediaPlayer1.Height) / 2 ;
+            RanPlay(g);
+			time.Interval = 1000 * 62;
+            time.Tick += new EventHandler(time_Tick);
+            time.Start();
+        }
+        private void RanPlay(int g)
+        {
+            if (g == 0 )
+            {
+                a= (int)r.Next(0, 2);
+                axWindowsMediaPlayer1.URL = ll[a];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            else if (g == 1 )
+            {
+                a = 2;
+                axWindowsMediaPlayer1.URL = ll[a];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            else if (g == 2)
+            {
+                a = (int)r.Next(3, 5);
+                axWindowsMediaPlayer1.URL = ll[a];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            else if (g == 3)
+            {
+                a = 5;
+                axWindowsMediaPlayer1.URL = ll[a];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+			int ppp = a + 4;
+            string[] q= { ppp.ToString() };
+            System.IO.File.AppendAllLines(@"C:\Users\sohyeon\Desktop\불러오기.txt", q);  //몇번 동영상인지 보냄
+        }
+        private void time_Tick(object sender, EventArgs e)
+        {
+            System.IO.File.Delete(@"C:\Users\sohyeon\Desktop\불러오기.txt");  //파일 삭제
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            this.Controls.Remove(axWindowsMediaPlayer1);
+            time.Stop();
+            Form9 showForm9 = new Form9();
+            showForm9.ShowDialog();
+            this.Visible = false;
+            Close();
+        }
+    }
+}

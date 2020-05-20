46996299e0c84df193f4b36b07c0c0887db3a71d
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-05-20 22:43:44 +0900

    감정저금통-ui

---
+++ ui/newForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace coc
+{
+	partial class newForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("조선일보명조", 30F, System.Drawing.FontStyle.Bold);
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(199)))), ((int)(((byte)(182)))));
+			this.label1.Location = new System.Drawing.Point(25, 334);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(1089, 51);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "아래의 카테고리 중 자신의 관심사를 생각해주세요";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(54, 35);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// newForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "newForm";
+			this.Text = "newForm";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
---
+++ ui/newForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coc
+{
+	public partial class newForm : Form
+	{
+		private System.Windows.Forms.Timer time = new System.Windows.Forms.Timer();
+		private int t = 0;
+		public newForm()
+		{
+			InitializeComponent();
+			this.Width = Screen.PrimaryScreen.Bounds.Width;
+			this.Height = Screen.PrimaryScreen.Bounds.Height;
+			this.Location = new Point(0, 0);
+			pictureBox1.ImageLocation = @"C:\Users\sohyeon\Desktop\소현\전공\신경공학(특별학기)\바름조_최종\image\new.png";
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+			pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+			label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+			label1.Top = (this.ClientSize.Height - label1.Height) / 2 - 400;
+			time.Interval = 1000;
+			time.Tick += new EventHandler(time_Tick);
+			time.Start();
+		}
+		private void time_Tick(object sender, EventArgs e) {
+			if (t == 5) {
+				time.Stop();
+				Form6 showForm6 = new Form6();
+				showForm6.ShowDialog();
+				this.Visible = false;
+				Close();
+			}
+			t++;
+		}
+	}
+}

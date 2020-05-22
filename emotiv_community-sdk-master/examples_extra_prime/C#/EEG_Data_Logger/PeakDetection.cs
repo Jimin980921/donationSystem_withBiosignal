b174c4b85fd717eaa4e1028535f594bb0ce8326e
sohyeon98720 <47767202+sohyeon98720@users.noreply.github.com>
2020-05-22 14:12:00 +0900

    감정저금통-뇌파데이터처리

---
+++ emotiv_community-sdk-master/examples_extra_prime/C#/EEG_Data_Logger/PeakDetection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace EEG_Data_Logger
+{
+    class PeakDetection
+	{
+		private double lastval=0;
+		public void Excute(double[] result)
+        {
+            var lastindex = 0;
+            bool dirpos = true;
+            for (int i = 0; i < result.Length; i++)
+            {
+                var currentval = result[i];
+                if (dirpos && currentval < lastval)
+                {
+                    //Console.WriteLine(string.Format("i: {0} value: {1}", lastindex, result[lastindex]));
+                    dirpos = false;
+                }
+
+                if (currentval >= lastval)
+                    dirpos = true;
+
+                lastval = currentval;
+                lastindex = i;	
+            }
+        }
+		public double GetValue()
+		{
+			return lastval;	
+		}
+
+	}
+}
